e616b4790fd600ed3a9813a7da97be505dd72f55
Lunchbox1188 <stevemott1188@gmail.com>
2024-05-12 12:03:01 +0100

    Changed splash background

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 AssetImage backgroundImage = AssetImage("assets/images/background.png");
 
 AssetImage splashImage = AssetImage("assets/images/splash.jpg");
+
+AssetImage newSplashImage = AssetImage("assets/images/boxer.png");
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:random_dog_api/constants/exports.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -10,18 +11,20 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 2), () => Get.offNamed('/home'));
+    // Future.delayed(Duration(seconds: 2), () => Get.offNamed('/home'));
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Loading...'),
+        child: Text('Loading...', style: TextStyle(), ),
+        heightFactor: 50,
+        widthFactor: 150,
       ),
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/images/splash.jpg'),
+          image: newSplashImage,
         ),
       ),
     );
