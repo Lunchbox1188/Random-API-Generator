6ef5722867e17e01d90af15501da2d9cfb9cce9e
Lunchbox1188 <stevemott1188@gmail.com>
2024-05-12 10:01:59 +0100

    Added images to assets

---
+++ lib/constants/images.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+AssetImage backgroundImage = AssetImage("assets/images/background.png");
+
+AssetImage splashImage = AssetImage("assets/images/splash.jpg");
